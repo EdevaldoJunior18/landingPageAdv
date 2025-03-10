7d34009a3b32062f2bf93f8566d30a70dba9c92e
edevaldo <edevaldo31_@hotmail.com>
2025-03-09 22:06:37 -0300

    fix: front details

--- src/pages/components/FirstNavbar/firstNavbar.tsx
+++ src/pages/components/FirstNavbar/firstNavbar.tsx
@@ -1,4 +1,4 @@
-import logoWhatsapp from "../../assets/images/logo_adv.avif";
+import logo from "../../assets/images/logo_adv.avif";
 
 export default function Navbar() {
     return (
@@ -6,7 +6,7 @@ export default function Navbar() {
             <div className="flex items-center justify-center w-full max-w-7xl">
                 <div className="flex items-center justify-center gap-4">
                     <div className="w-[80px] sm:w-[180px] h-[3px] bg-[#c4942c] rounded"></div>
-                    <img src={logoWhatsapp.src} alt="WhatsApp" className="w-28 h-28" />
+                    <img src={logo.src} alt="WhatsApp" className="w-28 h-28" />
                     <div className="w-[80px] sm:w-[180px] h-[3px] bg-[#c4942c] rounded"></div>
                 </div>
             </div>
--- src/pages/components/WhatsButton/whatsButton.tsx
+++ src/pages/components/WhatsButton/whatsButton.tsx
@@ -29,21 +29,10 @@ export default function WhatsButton() {
             target="_blank"
             rel="noopener noreferrer"
             className="fixed right-6 flex flex-col items-center z-50 transition-all duration-300"
-            style={{ bottom: bottomOffset }}
-        >
+            style={{ bottom: bottomOffset }} >
             <div className="bg-[#25D366] p-3 rounded-full shadow-lg hover:bg-[#20b858] transition-colors duration-300">
                 <img src={logoWhatsapp.src} alt="WhatsApp" className="w-8 h-8" />
             </div>
-            <span
-                className="text-white font-bold text-base mt-2"
-                style={{
-                    fontFamily: "Inria Sans, sans-serif",
-                    lineHeight: "12px",
-                    letterSpacing: "0%",
-                }}
-            >
-                {/* Contate um Vendedor */}
-            </span>
         </a>
     );
 }
